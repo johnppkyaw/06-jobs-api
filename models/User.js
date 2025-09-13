afaecd2972af08a9f52d9c04ceeef65246ed7403
John Kyaw <jjpyae@gmail.com>
2025-09-13 16:48:45 -0400

    add all routes

--- models/User.js
+++ models/User.js
@@ -5,20 +5,20 @@ const jwt = require('jsonwebtoken');
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'Please provide a name'],
+    required: [true, 'Please provide a name'],
     minlength: 3,
     maxlength: 50,
   },
   email: {
     type: String,
-    require: [true, 'Please provide an email'],
+    required: [true, 'Please provide an email'],
     match: [ /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email'
     ],
     unique: true
   },
   password: {
     type: String,
-    require: [true, 'Please provide a password'],
+    required: [true, 'Please provide a password'],
     minlength: 6
   }
 })
